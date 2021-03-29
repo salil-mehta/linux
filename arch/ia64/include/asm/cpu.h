39ba90919bad514e60d9469c272b0ea4fb801862
James Morse <james.morse@arm.com>
2021-03-29 13:06:14 +0100

    ia64/topology: Switch over to GENERIC_CPU_DEVICES
    ia64 has its own arch specific data structure for cpus: struct ia64_cpu.
    This has one member, making ia64's cpu_devices the same as that
    provided be GENERIC_CPU_DEVICES.
    ia64 craetes a percpu struct ia64_cpu called cpu_devices, which has no
    users. Instead it uses the struct ia64_cpu named sysfs_cpus allocated at
    boot.
    
    Remove the arch specific structure allocation and initialisation.
    ia64's arch_register_cpu() now overrides the weak version from
    GENERIC_CPU_DEVICES, and uses the percpu cpu_devices defined by
    core code.
    
    All uses of sysfs_cpus are changed to use the percpu cpu_devices.
    
    This is an intermediate step to the logic being moved to drivers/acpi,
    where GENERIC_CPU_DEVICES will do the work when booting with acpi=off.
    
    Signed-off-by: James Morse <james.morse@arm.com>

--- arch/ia64/include/asm/cpu.h
+++ arch/ia64/include/asm/cpu.h
@@ -7,12 +7,6 @@
 #include <linux/topology.h>
 #include <linux/percpu.h>
 
-struct ia64_cpu {
-	struct cpu cpu;
-};
-
-DECLARE_PER_CPU(struct ia64_cpu, cpu_devices);
-
 DECLARE_PER_CPU(int, cpu_state);
 
 #ifdef CONFIG_HOTPLUG_CPU
