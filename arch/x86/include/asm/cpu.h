1b9240365c628a5779b3659b379038f0a4f91111
James Morse <james.morse@arm.com>
2021-03-29 13:19:03 +0100

    x86/topology: Switch over to GENERIC_CPU_DEVICES
    Now that GENERIC_CPU_DEVICES calls arch_register_cpu(), which can be
    overridden by the arch code, switch over to this to allow common code
    to choose when the register_cpu() call is made.
    
    x86's struct cpus come from struct x86_cpu, which has no other members
    or users. Remove this and use the version defined by common code.
    
    This is an intermediate step to the logic being moved to drivers/acpi,
    where GENERIC_CPU_DEVICES will do the work when booting with acpi=off.
    
    Signed-off-by: James Morse <james.morse@arm.com>

--- arch/x86/include/asm/cpu.h
+++ arch/x86/include/asm/cpu.h
@@ -23,10 +23,6 @@ static inline void prefill_possible_map(void) {}
 
 #endif /* CONFIG_SMP */
 
-struct x86_cpu {
-	struct cpu cpu;
-};
-
 #ifdef CONFIG_HOTPLUG_CPU
 extern void arch_unregister_cpu(int);
 extern void start_cpu0(void);
