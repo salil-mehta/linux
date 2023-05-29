salil-mehta/linux

commit 346e7480f1d4740b3d798da60f83f087ea6488b4

drivers: clocksource: add CPU PM notifier for ARM architected timer
Few control settings done in architected timer as part of initialisation
can be lost when CPU enters deeper power states. They need to be
restored when the CPU is (warm)reset again.

This patch adds CPU PM notifiers to save the counter control register
when entering low power modes and restore it when CPU exits low power.

Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
Reviewed-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
Reviewed-by: Will Deacon <will.deacon@arm.com>
Acked-by: Olof Johansson <olof@lixom.net>
Signed-off-by: Sudeep KarkadaNagesha <sudeep.karkadanagesha@arm.com>

drivers/clocksource/arm_arch_timer.c
@@ -13,6 +13,7 @@
 #include <linux/device.h>
 #include <linux/smp.h>
 #include <linux/cpu.h>
+#include <linux/cpu_pm.h>
 #include <linux/clockchips.h>
 #include <linux/interrupt.h>
 #include <linux/of_irq.h>
@@ -475,6 +476,33 @@ static struct notifier_block arch_timer_cpu_nb = {
 	.notifier_call = arch_timer_cpu_notify,
 };
 
+#ifdef CONFIG_CPU_PM
+static unsigned int saved_cntkctl;
+static int arch_timer_cpu_pm_notify(struct notifier_block *self,
+				    unsigned long action, void *hcpu)
+{
+	if (action == CPU_PM_ENTER)
+		saved_cntkctl = arch_timer_get_cntkctl();
+	else if (action == CPU_PM_ENTER_FAILED || action == CPU_PM_EXIT)
+		arch_timer_set_cntkctl(saved_cntkctl);
+	return NOTIFY_OK;
+}
+
+static struct notifier_block arch_timer_cpu_pm_notifier = {
+	.notifier_call = arch_timer_cpu_pm_notify,
+};
+
+static int __init arch_timer_cpu_pm_init(void)
+{
+	return cpu_pm_register_notifier(&arch_timer_cpu_pm_notifier);
+}
+#else
+static int __init arch_timer_cpu_pm_init(void)
+{
+	return 0;
+}
+#endif
+
 static int __init arch_timer_register(void)
 {
 	int err;
@@ -514,11 +542,17 @@ static int __init arch_timer_register(void)
 	if (err)
 		goto out_free_irq;
 
+	err = arch_timer_cpu_pm_init();
+	if (err)
+		goto out_unreg_notify;
+
 	/* Immediately configure the timer on the boot CPU */
 	arch_timer_setup(this_cpu_ptr(arch_timer_evt));
 
 	return 0;
 
+out_unreg_notify:
+	unregister_cpu_notifier(&arch_timer_cpu_nb);
 out_free_irq:
 	if (arch_timer_use_virtual)
 		free_percpu_irq(arch_timer_ppi[VIRT_PPI], arch_timer_evt);

commit c487dd7d0be6437109ded7b90976025453050d04

clocksource/drivers/arm_arch_timer: Save/restore the enabled bits over idle
When a CPU enters low power mode using PSCI's CPU_SUSPEND, the 'pm'
notify chain is triggered. The arch-timer uses this to save/restore
the event stream configuration and the traps in CNTKCTL_EL1. It doesn't
save/restore the timer counter match or control register. The control
register is where the enable bit for the interrupt lives. Interrupts are
(re-)enabled when set_next_event is called, which happens when the timer
interrupt fires.

When CPUs return via the cold path with the MMU off, all the system
registers will have been reset. On Juno the timer configuration registers
reset value has the enabled bit clear.

The existing code depends on the timer interrupt fire-ing and being
latched by the interrupt controller - before the CPU powers up and
resets the timer control register. Linux takes the timer interrupt
after returning from idle, and re-configures the timer.

This is a problem when the timer interrupt wasn't the reason the CPU
woke from idle. The timer interrupt has been disabled when the CPU
got reset, and the timer interrupt is not pending in the interrupt
controller because that wasn't the reason the CPU woke from idle.
The result is all the CPUs sitting in the idle thread.

Add helpers to save/restore the counter value and the control register.
In most cases this will cause the timer interrupt to become pending
again, as its level triggered this is harmless.

Fixes: 346e7480f1d4 ("drivers: clocksource: add CPU PM notifier for ARM architected timer")
Signed-off-by: James Morse <james.morse@arm.com>
----
Discovered when implementing this in kvmtool (PSCI in user-space) with
idle-states that return immediately.

arch/arm64/include/asm/arch_timer.h
@@ -141,6 +141,8 @@ u64 arch_timer_reg_read_cp15(int access, enum arch_timer_reg reg)
 		switch (reg) {
 		case ARCH_TIMER_REG_CTRL:
 			return read_sysreg(cntp_ctl_el0);
+		case ARCH_TIMER_REG_CVAL:
+			return read_sysreg(cntp_cval_el0);
 		default:
 			BUILD_BUG();
 		}
@@ -148,6 +150,8 @@ u64 arch_timer_reg_read_cp15(int access, enum arch_timer_reg reg)
 		switch (reg) {
 		case ARCH_TIMER_REG_CTRL:
 			return read_sysreg(cntv_ctl_el0);
+		case ARCH_TIMER_REG_CVAL:
+			return read_sysreg(cntv_cval_el0);
 		default:
 			BUILD_BUG();
 		}

drivers/clocksource/arm_arch_timer.c
@@ -59,7 +59,7 @@
  */
 #define MIN_ROLLOVER_SECS	(40ULL * 365 * 24 * 3600)
 
-static unsigned arch_timers_present __initdata;
+static unsigned arch_timers_present __ro_after_init;
 
 struct arch_timer {
 	void __iomem *base;
@@ -1138,6 +1138,48 @@ static int arch_timer_dying_cpu(unsigned int cpu)
 }
 
 #ifdef CONFIG_CPU_PM
+static DEFINE_PER_CPU(unsigned long, saved_cntXctl);
+static DEFINE_PER_CPU(unsigned long, saved_cntXcval);
+
+static void save_timer_state(unsigned long type)
+{
+	int access;
+	bool virt_timer;
+	u64 cntXctl, cntXcval;
+
+	if (!(type & ARCH_TIMER_TYPE_CP15)) {
+		/* Only CPU timers may lose state */
+		return;
+	}
+
+	virt_timer = (arch_timer_uses_ppi == ARCH_TIMER_VIRT_PPI);
+	access = virt_timer ? ARCH_TIMER_VIRT_ACCESS : ARCH_TIMER_PHYS_ACCESS;
+	cntXctl = arch_timer_reg_read_cp15(access, ARCH_TIMER_REG_CTRL);
+	cntXcval = arch_timer_reg_read_cp15(access, ARCH_TIMER_REG_CVAL);
+
+	__this_cpu_write(saved_cntXctl, cntXctl);
+	__this_cpu_write(saved_cntXcval, cntXcval);
+}
+static void restore_timer_state(unsigned long type)
+{
+	int access;
+	bool virt_timer;
+	u64 cntXctl, cntXcval;
+
+	if (!(type & ARCH_TIMER_TYPE_CP15)) {
+		/* Only CPU timers may lose state */
+		return;
+	}
+
+	virt_timer = (arch_timer_uses_ppi == ARCH_TIMER_VIRT_PPI);
+	access = virt_timer ? ARCH_TIMER_VIRT_ACCESS : ARCH_TIMER_PHYS_ACCESS;
+	cntXctl = __this_cpu_read(saved_cntXctl);
+	cntXcval = __this_cpu_read(saved_cntXcval);
+
+	arch_timer_reg_write_cp15(access, ARCH_TIMER_REG_CVAL, cntXcval);
+	arch_timer_reg_write_cp15(access, ARCH_TIMER_REG_CTRL, cntXctl);
+}
+
 static DEFINE_PER_CPU(unsigned long, saved_cntkctl);
 static int arch_timer_cpu_pm_notify(struct notifier_block *self,
 				    unsigned long action, void *hcpu)
@@ -1145,9 +1187,13 @@ static int arch_timer_cpu_pm_notify(struct notifier_block *self,
 	if (action == CPU_PM_ENTER) {
 		__this_cpu_write(saved_cntkctl, arch_timer_get_cntkctl());
 
+		save_timer_state(arch_timers_present);
+
 		cpumask_clear_cpu(smp_processor_id(), &evtstrm_available);
 	} else if (action == CPU_PM_ENTER_FAILED || action == CPU_PM_EXIT) {
 		arch_timer_set_cntkctl(__this_cpu_read(saved_cntkctl));
+
+		restore_timer_state(arch_timers_present);
 
 		if (arch_timer_have_evtstrm_feature())
 			cpumask_set_cpu(smp_processor_id(), &evtstrm_available);